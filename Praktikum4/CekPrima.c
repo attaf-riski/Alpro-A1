866958b209336ef6adc5d97d726b014cf11a3573
Attaf Riski <69828152+attaf-riski@users.noreply.github.com>
2022-03-19 00:12:33 +0700

    revisi_1

--- Praktikum4/CekPrima.c
+++ Praktikum4/CekPrima.c
@@ -2,7 +2,7 @@
 Nama File : CekPrima.c
 Deskripsi : Cek suatu nilai integer apakah bilangan prima
 Pembuat : Attaf Riski Putra Ramadhan -- 24060121120005
-Tanggal : 14 Maret 2022
+Tanggal : 19 Maret 2022
 */
 
 
@@ -11,24 +11,44 @@ Tanggal : 14 Maret 2022
 
 int main()
 {
-	//kamus
+	//KAMUS
 	int i; // counter
+	int jumlahFaktor; // jumlah faktor N
 	int N; //tampungan nilai
 
-	//algoritma
+	//ALGORITMA
 	printf("Masukan nilai N : ");
 	scanf("%d",&N);
+	jumlahFaktor = 0; //jumlah faktor awal
 
-	for(i = 2;i<=N-1;i++)// 1 dan N pasti bisa membagi N
+	// dua kasus
+	if (N > 0 && N!=1)
 	{
-		// satu kasus
-		if (N % i == 0) // nilai i bukan 1 atau N
+		jumlahFaktor = 2; // 1 dan N sudah pasti faktor N
+		for(i = 2;i<=N-1;i++)// 1 dan N pasti bisa membagi N
 		{
-			printf("%d adalah bukan bilangan prima",N);
-			//program berhenti
-			return 0;
-		}
+			// satu kasus
+			if (N % i == 0) // ada faktor lain
+			{
+				jumlahFaktor += 1;
+
+		    }
+	    }
+
+
+	    if (jumlahFaktor == 2)
+	    {
+	    	printf("%d bilangan prima",N);
+	    }
+	    else // faktor N lebih dari 2 jumlahnya
+	    {
+	    	printf("%d bukan bilangan prima",N);
+	    }
+
 	}
-	printf("%d adalah bilangan prima",N);
+	else // N tidak memenuhi syarat > 0 atau N = 1
+	{
+		printf("%d bukan bilangan prima",N);
+	}
 	return 0;
 }
